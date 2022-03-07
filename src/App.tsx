90e3c58fe4357fb9b5923a947f759e6b0528b1ac
ryuki8643 <garyuki88me@gmail.com>
2022-03-07 18:17:44 +0900

    add gragh table

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from "react";
+import React, {  useState} from "react";
 import "./index.css"
 import Chart from "react-google-charts";
 
@@ -24,6 +24,7 @@ const data2 = [
     ["RU", 100]
 ];
 
+
 const dataStats={"first":data1,"second":data2}
 
 export default function App(){
@@ -31,15 +32,29 @@ export default function App(){
 
     const [dataset,setDataset]=useState(data1)
     var options = {
+        sizeAxis: { minValue: 100, maxValue: 100 },
 
         colorAxis: {colors: ['#00853f', 'black', '#FFFF00']},
+        legend:{textStyle: {color: 'black', fontSize: 30}},
+        tooltip:{textStyle: {fontSize: 30,bold:true}, showColorCode: true},
+
         backgroundColor: '#81d4fa',
         datalessRegionColor: '#f8bbd0',
         defaultColor: '#f5f5f5',
+        magnifyingGlass: {enable: true, zoomFactor: 7.5}
+
     };
+    var tableOption={
+        cssClassNames :{headerRow: 'bigAndBoldClass',
+            hoverTableRow: 'highlightClass'}
+    };
+    var pieChartOption={
+        chartArea:{left:'20%',top:20,width:'170%',height:'170%'},
+        fontSize:16
+    }
 
 
-    useEffect(()=>{console.log(dataset)},dataset)
+
     const dataChange =(e:React.ChangeEvent<HTMLSelectElement>) => {
         if(e.target.value=="first") {setDataset(data1)}
         else if(e.target.value=="second"){setDataset(data2)}
@@ -52,7 +67,13 @@ export default function App(){
             <option value="first" >first</option>
             <option value="second" >second</option>
           </select>
-          <Chart chartType="GeoChart" width="100%" height="500px" data={dataset} options={options} />
+          <Chart chartType="GeoChart" width="100%" height="500px" data={dataset} options={options}  />
+          <tr>
+          <th><Chart className="tableCLass" chartType="Table" width="100%" height="100%" data={dataset}  options={tableOption}/></th>
+
+          <th><Chart className="pieChartClass" chartType="PieChart" width="100%" height="100%" data={dataset}  options={pieChartOption}/></th>
+          </tr>
+
         </div>
     );
 
